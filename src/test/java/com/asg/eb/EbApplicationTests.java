4fbbd1943c90a3fcccf08db7fd8cbb274ad24942
suddiyo <suddiyo@naver.com>
2023-11-23 17:02:58 +0900

    Initial commit

---
+++ src/test/java/com/asg/eb/EbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asg.eb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
