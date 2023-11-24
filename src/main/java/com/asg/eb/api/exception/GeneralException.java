995d0d7d567713900843544c5333fa79ef80b023
suddiyo <suddiyo@naver.com>
2023-11-24 21:58:03 +0900

    [Feat/#7] GeneralException 작성

--- src/main/java/com/asg/eb/api/exception/GeneralException.java
+++ src/main/java/com/asg/eb/api/exception/GeneralException.java
@@ -1,4 +1,22 @@
 package com.asg.eb.api.exception;
 
-public class GeneralException {
+import com.asg.eb.api.code.BaseCode;
+import com.asg.eb.api.code.Reason;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseCode code;
+
+    public Reason getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public Reason getErrorReasonHttpStatus() {
+        return this.code.getReasonHttpStatus();
+    }
+
 }
